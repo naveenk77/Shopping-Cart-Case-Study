f9162d4c13585cd56b3c72ebc86b6a735ec12ac5
naveenk77 <kondanaveen77@gmail.com>
2022-01-02 11:23:19 +0530

    Api GateWay

---
+++ api-gateway/src/main/java/com/naveen/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,22 @@
+package com.naveen.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+	
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+}
